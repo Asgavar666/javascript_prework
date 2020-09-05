809ef380b17e4e3cb1ab137d7e90bf535614763e
Krzysztof Badelek <k.badelek@gmail.com>
2020-09-05 16:11:21 +0200

    Game_v1

--- js/script.js
+++ js/script.js
@@ -8,9 +8,70 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-let computerMove = 'kamień';
-printMessage('Zagrałem ' + 
-computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
-let playerMove = 'papier';
-printMessage('Zagrałes ' + playerMove + '! Jeśli mój ruch to nożyce, to wygrywam!');
+/*RUCH GRACZA*/
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+	playerMove = 'papier'
+}
+else{ playerMove = 'nożyce'};
+
+printMessage('Twój ruch to: ' + playerMove);
+
+/*RUCH KOMPUTERA */
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+	computerMove = 'papier';
+}
+else{
+	computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+/*LOGIKA GRY*/
+
+if(computerMove == 'kamień' && playerMove == 'kamień'){
+	printMessage('Remis');
+}
+else if(computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Wygrales!');
+}
+
+else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage('Przegrales!');
+}
+else if( computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Remis');
+}
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+	printMessage('Przegrales!');
+}
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Wygrales!')
+}
+else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+	printMessage('Remis');
+}
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Wygrales!');
+}
+else {
+	printMessage('Przegrales!')
+}
